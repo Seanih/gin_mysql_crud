1f89b2860b9169ce68105eee64ba880a17fbf9ac
Sean Anih <sean.anih@gmail.com>
2023-03-25 00:10:34 -0400

    changed from Fiber to Gin; established GET functionality

--- server/main.go
+++ server/main.go
@@ -4,28 +4,40 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/go-sql-driver/mysql"
-	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 )
 
 var db *sql.DB
 
+type Task struct {
+	TaskID    uint16 `json:"id"`
+	TaskName  string `json:"task_name"`
+	Completed bool   `json:"completed"`
+	OwnerName string `json:"owner_name"`
+}
+
 func main() {
 	dbConnection()
 
-	app := fiber.New()
-	port := ":4000"
+	app := gin.Default()
+	port := "localhost:4000"
 
-	fiberTest := fmt.Sprintf("server running on port%v", port)
+	app.GET("/", func(ctx *gin.Context) {
+		tasks, err := getAllTasks()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(fiberTest)
+		if err != nil {
+			log.Fatal(err)
+		}
+		ctx.IndentedJSON(http.StatusOK, tasks)
 	})
 
-	log.Fatal(app.Listen(port))
+	app.Run(port)
+
 }
 
 func dbConnection() {
@@ -59,4 +71,45 @@ func dbConnection() {
 	}
 
 	fmt.Println("successfully connected to db!")
+}
+
+func getAllTasks() ([]Task, error) {
+	rows, err := db.Query(("SELECT id, task_name, completed, owner_name FROM tasks JOIN owners ON tasks.owner_id = owners.owner_id"))
+
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var allTasks []Task
+
+	for rows.Next() {
+		var task Task
+		if err := rows.Scan(&task.TaskID, &task.TaskName, &task.Completed, &task.OwnerName); err != nil {
+			return allTasks, err
+		}
+
+		allTasks = append(allTasks, task)
+	}
+
+	if err = rows.Err(); err != nil {
+		return allTasks, err
+	}
+
+	return allTasks, nil
+}
+
+func addTask(newTask Task) (int64, error) {
+	result, err := db.Exec("INSERT INTO owners (owner_name) VALUES (?)", newTask.OwnerName)
+
+	if err != nil {
+		return 0, fmt.Errorf("there was an error: %v", err)
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("there was an error: %v", err)
+	}
+
+	return id, nil
 }
